5ca02cfffbed177cc70415145e86807c687d448a
Marine LB <marine.leborgne@live.fr>
2019-01-21 22:04:02 +0100

    add more features

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Marine Le Borgne, yet another developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Hello world, come visit.' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
@@ -19,7 +19,7 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#f00' },
 
   /*
   ** Global CSS
@@ -34,7 +34,16 @@ module.exports = {
   /*
   ** Nuxt.js modules
   */
-  modules: [],
+  modules: [
+    [
+      'nuxt-sass-resources-loader',
+      [
+        'assets/scss/global.scss',
+        'assets/scss/breakpoints.scss',
+        'assets/scss/mixins.scss'
+      ]
+    ],
+  ],
 
   /*
   ** Build configuration
